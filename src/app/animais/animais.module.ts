1c47b7c66c09ebd13e61f83c34865e8a91db9893
Igor Gripp <igorgripp@gmail.com>
2022-03-21 14:46:00 -0300

    Curso de Angular - Aula 2 - ALURA

---
+++ src/app/animais/animais.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimaisRoutingModule } from './animais-routing.module';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+
+
+@NgModule({
+  declarations: [
+    ListaAnimaisComponent
+  ],
+  imports: [
+    CommonModule,
+    AnimaisRoutingModule
+  ],
+  exports: [ListaAnimaisComponent]
+})
+export class AnimaisModule { }
